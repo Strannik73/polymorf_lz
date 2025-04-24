1efd4acef2992f6f1d3692094b9d411c79f55c32
Strannik37 <Santon@tutamail.com>
2025-04-24 21:14:39 +0300

    summoper.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from summoper import sumop
+
+def main():
+    smop = sumop('var3.csv')
+    smop.dann()
+
+if __name__ == '__main__':
+    main()
--- summoper.py
+++ summoper.py
@@ -1,19 +1,37 @@
 import pandas as pd
+#from __ import __
 
 
 class sumop:
 
-    def __init__(self):
+    def __init__(self, data): 
+        self.data = data
         self.df = pd.read_csv('var3.csv')
-
+       
     def __neg__(self):
-        return self.df.drop_duplicates()
-    print('количество удаленных дубликатов:  ' ,  ,)
+        return  self.df.drop_duplicates()  
+    
 
     def dann(self):
-        df = -self.df
-        
-    def delen(self):
-        df1 = self.df[self.df['Сумма cash-back'] <= 1000.00]    # строки, удовлетворяющие условию
-        df2 = self.df[self.df['Сумма cash-back'] > 1000.00]  
+        self.df = sumop('var3.csv')
 
+        self.df = -self.df
+
+        self.df1 = self.df[self.df['Сумма cash-back'] <= 1000.00]    
+        self.df2 = self.df[self.df['Сумма cash-back'] > 1000.00]  
+
+        print('количество удаленных строк:  ' ,100001 - (len(self.df1) + len(self.df2)),)
+        self.df1.to_csv('df1.csv')
+        self.df2.to_csv('df2.csv')
+        
+
+    def __del__(self):
+        print('удаление')
+
+
+def main():
+    smop = sumop('var3.csv')
+    smop.dann()
+
+if __name__ == '__main__':
+    main()
